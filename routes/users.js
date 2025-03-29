07ab133be79f06fdab5423b4c6080cffcba72986
Rahul Sohandani <rsohandani@gmail.com>
2025-03-29 18:05:56 -0400

    error-resolve

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 import express from "express";
+import bcrypt from "bcrypt";
 import { createUser, getUser, updateUser, deleteUser } from "../data/users.js";
 
 const router = express.Router();
@@ -6,10 +7,12 @@ const router = express.Router();
 router.route("/").post(async (req, res) => {
     try {
         const { name, email, password, age, height, weight, bmi, location } = req.body;
+        // console.log(req.body);
         const hashedPassword = await bcrypt.hash(password, 10);
         const user = await createUser(name, email, hashedPassword, age, height, weight, bmi, location);
         return res.status(201).json(user);
     } catch (e) {
+        console.log(e);
         return res.status(400).json({ error: e});
     }
 });
